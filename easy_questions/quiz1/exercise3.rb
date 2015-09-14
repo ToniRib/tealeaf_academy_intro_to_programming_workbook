0dc6764b387acc13cddf7dc5fdae1ca5f9b5c037
ToniRib <tonimarierib@gmail.com>
2015-09-13 19:34:01 -0600

    Adds solution to exercise 3 of quiz 1 in easy questions

---
+++ easy_questions/quiz1/exercise3.rb
@@ -0,0 +1,10 @@
+# Easy Questions - Quiz 1 - Exercise 3
+
+# initialize the string
+advice = "Few things in life are as important as house training your pet dinosaur."
+
+# replace the word 'important' with 'urgent'
+advice.sub!("important", "urgent")
+
+# print out the string
+puts advice
